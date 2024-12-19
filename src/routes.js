dbd4317239fae323fee6842c7a65a56f3d9bc355
jmlee9707 <jmlee9707@naver.com>
2024-12-19 17:59:57 +0900

    ⚡️ feat: hash router 구현

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,16 @@ class Router {
     this.routes[path] = component;
   }
 
-  router(path) {
+  getPath() {
+    if (window.location.hash) {
+      console.log(window.location.hash);
+      return window.location.hash.replace("#", "");
+    } else return window.location.pathname;
+  }
+
+  // 1
+  router() {
+    const path = this.getPath();
     const currentPath = this.matchUrl(path);
     const redirectUser = this.userRoute(currentPath);
     if (redirectUser) return;
@@ -30,12 +39,16 @@ class Router {
 
   renderPage(path) {
     const pageClass = this.routes[path];
+    const $app = document.querySelector("#root");
+    $app.innerHTML = "";
 
     if (pageClass) {
-      const $app = document.querySelector("#root");
-      $app.innerHTML = "";
       const $page = new pageClass("#root");
       $page.render();
+    } else {
+      const Not = this.routes["/404"];
+      const $not = new Not("#root");
+      $not.render();
     }
   }
 
@@ -54,8 +67,12 @@ class Router {
   }
 
   navigateTo(path) {
-    window.history.pushState(null, "", path);
-    this.router(path);
+    if (path.startsWith("#")) {
+      window.location.hash = path.replace("#", "");
+    } else {
+      window.history.pushState(null, "", path);
+      this.router();
+    }
   }
 
   matchUrl(path) {
@@ -66,12 +83,17 @@ class Router {
 
   initRoute() {
     // 초기화 및 이벤트 설정
-    this.router(window.location.pathname);
+    this.router();
 
     window.addEventListener("popstate", () => {
-      this.router(window.location.pathname);
+      if (!window.location.hash) this.router();
     });
 
+    window.addEventListener("hashchange", () => {
+      this.router();
+    });
+
+    // button, navbar 이동 처리
     window.addEventListener("click", (e) => {
       if (e.target.tagName === "A" && e.target.getAttribute("href")) {
         e.preventDefault();
